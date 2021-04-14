e9ca1e8243aff3b4b3d1bee4b86ff26811bfb14f
raghav94603 <34892373+raghav94603@users.noreply.github.com>
2021-04-14 23:29:49 +0530

    Create fun.h

---
+++ 3_Implementation/inc/fun.h
@@ -0,0 +1,61 @@
+/**
+ * @file fun.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-14
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __FUN_H__
+#define __FUN_H__
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/**
+ * @brief 
+ * 
+ * @return char** 
+ */
+
+
+char** boardcreation();
+
+/**
+ * @brief 
+ * 
+ * @param Board 
+ */
+void print(char** Board);
+
+/**
+ * @brief 
+ * 
+ * @param Board 
+ * @return int 
+ */
+int isdraw(char** Board);
+/**
+ * @brief 
+ * 
+ * @param Board 
+ * @param i 
+ * @param j 
+ * @return char 
+ */
+
+/**
+ * @brief 
+ * 
+ * @param Board 
+ * @param i 
+ * @param j 
+ * @return char 
+ */
+char winningmove(char** Board, int i, int j);
+
+#endif
